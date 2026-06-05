8f64b458c6279093abe85142c56b42becfdf564a
Owen <owenchan39@gmail.com>
2026-06-05 09:16:44 +0100

    init

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,1 @@
+return { "rebelot/kanagawa.nvim", enabled = false,version = "*", config = function() require("kanagawa").load("wave") end,  }
---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,3 @@
+return {
+    { "folke/tokyonight.nvim", enabled = true, config = function() vim.cmd.colorscheme "tokyonight" end }
+}
