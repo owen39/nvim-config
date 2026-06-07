1ed53563fa8d4910b9c989d21c79dd5e72060411
Owen <owenchan39@gmail.com>
2026-06-08 00:41:32 +0100

    update

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
 require("config.lazy")
+vim.opt.relativenumber = true
+vim.opt.number = true
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,3 +1,7 @@
 return {
-    { "folke/tokyonight.nvim", enabled = true, config = function() vim.cmd.colorscheme "tokyonight" end }
+    { "folke/tokyonight.nvim", enabled = true, config = function()
+        vim.cmd.colorscheme "tokyonight"
+        vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#ff9e64' })
+        vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#7aa2f7' })
+    end }
 }
