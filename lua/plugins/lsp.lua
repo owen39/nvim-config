32d8bafe3b011813fed3778c9931558043e3c65e
owen-gl <owen.chan@goodlord.co>
2026-06-08 13:33:03 +0100

    set up TS lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,15 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function ()
-		vim.lsp.enable("gopls")
-	end,
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.enable("gopls")
+		end,
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		config = function()
+			require("typescript-tools").setup({})
+		end,
+	},
 }
