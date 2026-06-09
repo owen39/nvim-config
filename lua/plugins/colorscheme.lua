7416786adf1d3a378109849f347e6c2607c579b8
owen-gl <owen.chan@goodlord.co>
2026-06-09 23:54:50 +0100

    did loads of stuff but sticked with tokyonight(mare)

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,75 @@
+local function set_defaults() 
+	vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#ff9e64' })
+	vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#7aa2f7' })	
+end
+
+return {
+	{ 
+		"folke/tokyonight.nvim", 
+		enabled = true, 
+		config = function()
+			vim.cmd.colorscheme "tokyonight"
+			set_defaults()
+		end
+	},
+	{ 
+		'projekt0n/github-nvim-theme', 
+		name = 'github-theme',   
+		enabled = false,
+		config = function()
+			require('github-theme').setup({
+			})
+
+			vim.cmd('colorscheme github_dark')
+		end,
+	},
+	{
+		"rockyzhang24/arctic.nvim",
+		enabled = false,
+		dependencies = { "rktjmp/lush.nvim" },
+		name = "arctic",
+		branch = "main",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme arctic")
+			set_defaults()
+		end
+	},
+	{
+		"rebelot/kanagawa.nvim", 
+		enabled = false,
+		version = "*", 
+		config = function() 
+			require("kanagawa").load("dragon") 
+			set_defaults()
+		end,  
+	},
+	{
+		"catppuccin/nvim", 
+		enabled = false, 
+		name = "catppuccin", 
+		priority = 1000,
+		config = function() 
+			vim.cmd.colorscheme "catppuccin-nvim"
+			set_defaults()
+		end,
+	},
+	{
+		"Mofiqul/vscode.nvim", 
+		enabled = false, 
+		config = function()
+			require('vscode').setup({})
+			vim.cmd.colorscheme "vscode"
+			set_defaults()
+		end,
+
+	},
+	{
+		"askfiy/visual_studio_code",
+		enabled = false,
+		config = function()
+			vim.cmd([[colorscheme visual_studio_code]])
+			set_defaults()
+		end,
+	},
+}
--- lua/plugins/kanagawa.lua
+++
@@ -1,1 +0,0 @@
-return { "rebelot/kanagawa.nvim", enabled = false,version = "*", config = function() require("kanagawa").load("wave") end,  }
