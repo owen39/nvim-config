ad82b50791e9818061a7b17850d792a58441134d
owen-gl <owen.chan@goodlord.co>
2026-06-09 22:13:22 +0100

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.diagnostic.config( { virtual_text = true } )
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,8 +6,25 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function ()
+	    require('telescope').setup {
+		    defaults = {
+			    path_display = "truncate"
+		    },
+		    pickers = {
+			    find_files = {
+				    theme = "ivy"
+			    }
+		    },
+		    extensions = {
+			    fzf = {}
+		    }
+	    }
+
+	    require('telescope').load_extension('fzf')
+
 	    local builtin = require('telescope.builtin')
 	    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+	    vim.keymap.set('n', '<leader>fp', builtin.git_files, { desc = 'Telescope find git files' })
 	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 	    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
